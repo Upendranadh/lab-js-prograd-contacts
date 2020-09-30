32817da19d108c2307f6dd7f76e12bf1900c9747
upendranadh <n.upendranadh@gmail.com>
2020-10-01 00:40:15 +0530

    FSJ0620C102

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
 //Enter your code here..
+//Dom elements
+const button = document.getElementById("btnGet");
+const message = document.getElementById("message");
+
+// Function which makes an API call
+const apiCall = () => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://jsonplaceholder.typicode.com/users");
+    request.addEventListener("readystatechange", () => {
+      if (request.status == 200 && request.readyState === 4) {
+        resolve(request.response);
+      } else if (request.readyState === 4) {
+        reject("Error fetching the data");
+      }
+    });
+    request.onerror = () => {
+      reject("erorr from the server");
+    };
+    // request.onerror(() => {
+    //   reject("erorr from the server");
+    // });
+
+    request.send();
+  });
+};
+
+button.addEventListener("click", () => {
+  let promise = apiCall();
+  promise
+    .then((data) => {
+      let parsedData = JSON.parse(data);
+      console.log(parsedData);
+      let player = "";
+      parsedData.forEach((user) => {
+        player += `<div class="player">
+          <div class="strength">Name : ${user.name}</div>
+          <div>Email   : ${user.email}</div>
+          <div>Phone   : ${user.phone}</div>
+          <div>Website : ${user.website}</div>
+          <div>Company : ${user.company.name}</div>
+          <div>City    : ${user.address.city}</div>
+          <div>Zipcode : ${user.address.zipcode}</div>
+         </div>`;
+      });
+      message.insertAdjacentHTML("beforeend", player);
+      return apiCall();
+    })
+    .then((data) => {
+      let parsedData = JSON.parse(data);
+      console.log(parsedData);
+    }) //if i want to chain promises
+    .catch((error) => {
+      console.log(error);
+    });
+});
